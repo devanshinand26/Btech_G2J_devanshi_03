a08bb7ef7e097b4bda80ea3444b2f19b8bb57a20
devanshinand26 <devanshinand8@gmail.com>
2026-02-11 10:25:06 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x, y, z;
+    printf("I am in section G2J\n");
+    printf("Enter two numbers %d,%d\n",x,y);
+    scanf("%d%d",&x,&y);
+    z = x + y;
+    printf("Sum of two numbers is %d",z);
+    return 0;
+}
+
