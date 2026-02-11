1e5aea7b9efbc2614de142109fdfabf08bca8628
devanshinand26 <devanshinand8@gmail.com>
2026-02-11 11:37:25 +0530

    Add files via upload

---
+++ Greatest of three no.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main()
+{
+    int month;
+    printf("Enter the value of month\n");
+    scanf("%d", &month);
+    if(month == 1)
+   { printf("January");
+   }
+    else if (month == 2)
+   { printf("FEB");
+   }
+   else if (month == 3)
+   { printf("March");
+   }
+   else if(month == 4)
+   { printf("April");
+   }
+    else if(month == 5)
+   { printf("May");
+   }
+    else if(month == 6)
+   { printf("June");
+   }
+    else if(month == 7)
+   { printf("July");
+   }
+    else if(month == 8)
+   { printf("August");
+   }
+    else if(month == 9)
+   { printf("September");
+   }
+    else if(month == 10)
+   { printf("OCT");
+   }
+    else if(month == 11)
+   { printf("NOV");
+   }
+    else if(month == 12)
+   { printf("DEC");
+   }
+ return 0;
+}
+   
+   
