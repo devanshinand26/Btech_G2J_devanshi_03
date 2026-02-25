40fe9233e23b1d499a51a1a310d6253b2f7cda19
devanshinand26 <devanshinand8@gmail.com>
2026-02-25 12:06:19 +0530

    Create Age criteria.c

---
+++ Age criteria.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int age;
+printf("Enter your age-\t");
+scanf("%d",&age);
+if(age>= 18)
+printf("You are eligible");
+else
+printf("You do not meet the elibility criteria");
+return 0;
+}
+    
+ 
